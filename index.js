d65a118d3f28b1f1825e02fe7caff8e49b36f568
kfrFahim <kfrfahim3@gmail.com>
2023-07-24 22:23:15 +0600

    reviews

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(express.json());
 
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.6eoz53h.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -28,6 +28,8 @@ async function run() {
     //await client.connect();
 
 const collegeCollection = client.db("collegeDB").collection("colleges");
+const admissionCollection = client.db("collegeDB").collection("admissions");
+const reviewCollection = client.db("collegeDB").collection("reviews");
 
     // Colleges
     app.get("/colleges", async (req, res) => {
@@ -35,6 +37,34 @@ const collegeCollection = client.db("collegeDB").collection("colleges");
      res.send(result);
    });
 
+   app.get("/colleges/:id" ,async(req,res)=>{
+     const id = req.params.id
+     const query = {_id: new ObjectId(id)};
+     const result = await collegeCollection.findOne(query)
+     res.send(result)
+   })
+
+  //  Checkout for admission
+   app.post("/admissions", async (req, res) => {
+    const body = req.body;
+    const result = await admissionCollection.insertOne(body);
+    res.send(result);
+    console.log(body);
+  });
+
+  // Get admission data
+  app.get("/admission", async (req, res) => {
+    const cursos = admissionCollection.find();
+    const result = await cursos.toArray();
+    res.send(result);
+  });
+
+
+   // review related apis
+   app.get('/reviews', async (req, res) => {
+    const result = await reviewCollection.find().toArray();
+    res.send(result);
+  })
 
 
 
